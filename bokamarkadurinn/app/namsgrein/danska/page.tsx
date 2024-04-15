96d7e060929fb30a5b3735e10d19c9385d3043d8
KristjanOmar <117899282+KristjanOmar@users.noreply.github.com>
2024-04-15 10:19:30 +0000

    basic routing

---
+++ bokamarkadurinn/app/namsgrein/danska/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main>
+            <h1>Danska</h1>
+        </main>
+    );
+}
---
+++ bokamarkadurinn/app/profill/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main>
+            <h1>Prófíll</h1>
+        </main>
+    );
+}
