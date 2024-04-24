dc7dfa7dafb5cd64bb3dc74a96524364571f1f45
KristjanOmar <117899282+KristjanOmar@users.noreply.github.com>
2024-04-24 12:33:17 +0000

    setti dummy data

---
+++ bokamarkadurinn/app/[namsgrein]/page.tsx
@@ -0,0 +1,11 @@
+
+// Dummy data -- Gögn koma síðar úr DB
+let adalNamsgreinar = ['Stærðfræði', 'Íslenska', 'Enska', 'Danska'];
+const aukaNamsgreinar = ['Náttúrufræði', 'Saga'];
+
+
+export default function Page() {
+    return (
+        <h1>Námsgreinarsíða</h1>
+    );
+}
