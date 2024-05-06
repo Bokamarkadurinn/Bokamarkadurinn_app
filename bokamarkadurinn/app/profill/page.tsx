c2e15060a55fd2862c021caf4614dad4f1dec8a9
Emilb05 <emilhal21@tskoli.is>
2024-05-06 11:52:24 +0000

    gengur vel

--- bokamarkadurinn/app/profill/page.tsx
+++ bokamarkadurinn/app/profill/page.tsx
@@ -1,9 +1,10 @@
 "use client"
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
 import './profill.css';
 
 export default function Page() {
-    const history = useHistory();
+    const [fratOr, setFratOr] = useState(false);
+    const [minarOr, setMinarOr] = useState(false);
     const saekjaMynd = () => {
         const mynd = "";
         if (mynd != "") {
@@ -17,12 +18,50 @@ export default function Page() {
         if (nafn != "") {
             return nafn;
         } else {
-            /* hér er leið til að fara inná innskráningar síðuna ef það er ekkert notendanafn
-            history.push('./innskraning');
+            /*// hér er leið til að fara inná innskráningar síðuna ef það er ekkert notendanafn
+            window.location.href = "./innskraning";
             return null;
             */
             return "Jón Jónsson";// þetta er bara til að testa, síðan ætti að fara á innskráningar síðuna ef það er ekkert notendarnafn
         }
+    }
+    const saekjaFraBaekur = () => {
+        const fraBaekur: string[] = [];
+
+        if (fraBaekur.length === 0) {
+            return "ekkert";
+        } else {
+            return (
+                <select>
+                    {fraBaekur.map((item, index) => (
+                        <option key={index}>{item}</option>
+                    ))}
+                </select>
+            );
+        }
+    }
+
+    const saekjaMinarBaekur = () => {
+        const minarBaekur: string[] = [];
+
+        if (minarBaekur.length === 0) {
+            return "ekkert";
+        } else {
+            return (
+                <select>
+                    {minarBaekur.map((item, index) => (
+                        <option key={index}>{item}</option>
+                    ))}
+                </select>
+            );
+        }
+    }
+    const toggleFratOr = () => {
+        setFratOr(!fratOr);
+    }
+
+    const toggleMinarOr = () => {
+        setMinarOr(!minarOr);
     }
     return (
         <main>
@@ -30,6 +69,22 @@ export default function Page() {
             <img src={saekjaMynd()} id="mynd" />
             <p id='nafn'>
                 {saekjaNafn()}
+            </p>
+            <p id='dropdown' onClick={toggleFratOr}>
+                Fráteknar bækur {fratOr ? "↓" : "→"} <br></br>
+                {fratOr && (
+                    <select>
+                        {saekjaFraBaekur()}
+                    </select>
+                )}
+            </p>
+            <p id='dropdown' onClick={toggleMinarOr}>
+                Mínar bækur {minarOr ? "↓" : "→"} <br></br>
+                {minarOr && (
+                    <select>
+                        {saekjaMinarBaekur()}
+                    </select>
+                )}
             </p>
         </main>
     );
