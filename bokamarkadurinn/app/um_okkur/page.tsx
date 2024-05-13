2003998c5a483798b2ed08ead9012ae0f5c01628
Emilb05 <emilhal21@tskoli.is>
2024-05-13 10:26:46 +0000

    laga texta

--- bokamarkadurinn/app/um_okkur/page.tsx
+++ bokamarkadurinn/app/um_okkur/page.tsx
@@ -6,11 +6,12 @@ export default function Home() {
       <title>Um okkur</title>
       <h1 id="head">Um okkur</h1>
       <div id='texti'>
-        Við erum hópur af strákum sem er að gera þetta sem verkefni í vefforitun.
-         Þessi síða er gerð til að laga vandamál sem datt í hug, 
-         vandamálið er þannig að það er erfitt að vita hvaða bók er til og hvar. 
-         Síðan á að hjálpa til með að finna bókina sem maður er að leita af 
-         og taka hana frá svo maður gæti farið og keypt það eða fengið það lánað.
+        <br />
+        <p>Við erum hópur af strákum sem er að gera þetta sem verkefni í vefforitun.</p><br />
+        <p>Þessi síða er gerð til að laga vandamál sem datt í hug, 
+         vandamálið er þannig að það er erfitt að vita hvaða bók er til og hvar. </p><br />
+        <p>Síðan á að hjálpa til með að finna bókina sem maður er að leita af 
+         og taka hana frá svo maður gæti farið og keypt það eða fengið það lánað.</p><br />
       </div>
     </main>
   );
